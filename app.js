ab842cfe240db375eb7cc0ccc2a2b6b8df5a3332
Leonardo Mauricio <leomauricio7@gmail.com>
2020-03-17 09:08:52 -0300

    add confi transport socket.io

--- app.js
+++ app.js
@@ -17,9 +17,6 @@ const io = require('socket.io')(http);
 //io.origins(['http://sigap.site']);
 io.origins(['*']);
 
-io.set("transports", ["xhr-polling"]); 
-io.set("polling duration", 10); 
-
 io.on('connection', function(socket) {
     console.log('a user connected:'+socket.id);
     socket.on('disconnect', function() {
